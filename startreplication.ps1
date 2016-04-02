a28f59bdcbce095cb9107a2a609847fdd3f72ae3
happek1 <happek@gmail.com>
2016-04-01 21:59:29 -0400

    Added files via upload

---
+++ startreplication.ps1
@@ -0,0 +1,5 @@
+﻿$rs = Get-Content .\drexch.txt | New-PSSession
+invoke-Command -Session $rs -ScriptBlock {net start spooler} -AsJob
+Sleep -Seconds 5
+invoke-Command -ScriptBlock {gsv spooler} -Session $rs | Format-Table
+get-pssession | remove-pssession
