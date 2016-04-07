48c5ae2e0488617a8cc7a572b0ecb25ecd52ec74
happek1 <happek@gmail.com>
2016-04-07 19:46:17 -0400

    Update stopreplication.ps1

--- stopreplication.ps1
+++ stopreplication.ps1
@@ -1,5 +1,5 @@
 #Description
-#Creates sessions to servers specificed in a text file and stops a service, then checks and outputs the status
+#Creates sessions to servers specificed in a text file and starts a service, then checks and outputs the status
 ﻿$rs = Get-Content .\drexch.txt | New-PSSession
 invoke-Command -Session $rs -ScriptBlock {net stop spooler} -AsJob
 Sleep -Seconds 5
